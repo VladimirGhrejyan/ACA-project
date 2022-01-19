e46a8d8967856addaddd552f67ba51fa2b83fe76
Vladimir_Ghrejyan <vladimir.ghrejyan@gmail.com>
2022-01-19 23:43:38 +0400

    header bug

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,135 +1,55 @@
 import React from 'react'
-import { useState } from 'react'
 import "./layout.css"
-import { AppBar, Toolbar, Container, Box, IconButton, Typography, Menu, Avatar, Button, Tooltip, MenuItem } from "@mui/material"
+import { AppBar, Toolbar, Container, Box, IconButton, Typography, Button } from "@mui/material"
 import MenuIcon from '@mui/icons-material/Menu';
+import { makeStyles } from '@mui/styles'
+import {NavLink} from 'react-router-dom';
 
 function Header() {
-    const pages = ['Products', 'Pricing', 'Blog'];
-    const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+    const useStyles = makeStyles((theme) => (
+        {
+            root: {
+                flexGrow: 1
+            },
+            menuButton: {
+                marginRight: theme.spacing(1)
+            },
+            title: {
+                flexGrow: 1
+            }
+        }
+    ) 
+    )
 
-    const [anchorElNav, setAnchorElNav] = useState(null);
-    const [anchorElUser, setAnchorElUser] = useState(null);
-
-    const handleOpenNavMenu = (event) => {
-        setAnchorElNav(event.currentTarget);
-    };
-  
-    const handleOpenUserMenu = (event) => {
-        setAnchorElUser(event.currentTarget);
-    };
-
-    const handleCloseNavMenu = () => {
-        setAnchorElNav(null);
-    };
-
-    const handleCloseUserMenu = () => {
-        setAnchorElUser(null);
-    };
-    
+    const classes = useStyles();
     return (
-        
+
         <div className='header1'>
 
                 <AppBar position="static">
-                <Container maxWidth="xl">
-                    <Toolbar disableGutters>
-                    <Typography
-                        variant="h6"
-                        noWrap
-                        component="div"
-                        sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
-                    >
-                        LOGO
-                    </Typography>
-
-                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-                        <IconButton
-                        size="large"
-                        aria-label="account of current user"
-                        aria-controls="menu-appbar"
-                        aria-haspopup="true"
-                        onClick={handleOpenNavMenu}
-                        color="inherit"
-                        >
-                        <MenuIcon />
-                        </IconButton>
-                        <Menu
-                        id="menu-appbar"
-                        anchorEl={anchorElNav}
-                        anchorOrigin={{
-                            vertical: 'bottom',
-                            horizontal: 'left',
-                        }}
-                        keepMounted
-                        transformOrigin={{
-                            vertical: 'top',
-                            horizontal: 'left',
-                        }}
-                        open={Boolean(anchorElNav)}
-                        onClose={handleCloseNavMenu}
-                        sx={{
-                            display: { xs: 'block', md: 'none' },
-                        }}
-                        >
-                        {pages.map((page) => (
-                            <MenuItem key={page} onClick={handleCloseNavMenu}>
-                            <Typography textAlign="center">{page}</Typography>
-                            </MenuItem>
-                        ))}
-                        </Menu>
-                    </Box>
-                    <Typography
-                        variant="h6"
-                        noWrap
-                        component="div"
-                        sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
-                    >
-                        LOGO
-                    </Typography>
-                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-                        {pages.map((page) => (
-                        <Button
-                            key={page}
-                            onClick={handleCloseNavMenu}
-                            sx={{ my: 2, color: 'white', display: 'block' }}
-                        >
-                            {page}
-                        </Button>
-                        ))}
-                    </Box>
-
-                    <Box sx={{ flexGrow: 0 }}>
-                        <Tooltip title="Open settings">
-                        <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                            <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-                        </IconButton>
-                        </Tooltip>
-                        <Menu
-                        sx={{ mt: '45px' }}
-                        id="menu-appbar"
-                        anchorEl={anchorElUser}
-                        anchorOrigin={{
-                            vertical: 'top',
-                            horizontal: 'right',
-                        }}
-                        keepMounted
-                        transformOrigin={{
-                            vertical: 'top',
-                            horizontal: 'right',
-                        }}
-                        open={Boolean(anchorElUser)}
-                        onClose={handleCloseUserMenu}
-                        >
-                        {settings.map((setting) => (
-                            <MenuItem key={setting} onClick={handleCloseNavMenu}>
-                            <Typography textAlign="center">{setting}</Typography>
-                            </MenuItem>
-                        ))}
-                        </Menu>
-                    </Box>
-                    </Toolbar>
-                </Container>
+                    <Container fixed>
+                        <Toolbar>
+                            <IconButton
+                            edge="start" color="inherit" aria-laabel="menu" className={classes.menuButton}
+                            >
+                                <MenuIcon />
+                            </IconButton>
+                            <Typography
+                            variant="h6" className={classes.title} 
+                            >
+                                Our Project
+                            </Typography>
+                            <Box
+                            mr={3}
+                            >
+                                <Button
+                                color="inherit" variant="outlined"
+                                >
+                                    <NavLink to="/signin">Sign In </NavLink>
+                                </Button>
+                            </Box>
+                        </Toolbar>
+                    </Container>
                 </AppBar>
 
         </div>
